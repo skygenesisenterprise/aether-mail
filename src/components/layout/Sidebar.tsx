44f1d4f5a71afc71dbcbeebd45a6c8d9ab950da6
liamvnastoria <liam.dispa@skygenesisenterprise.com>
2025-04-25 20:24:45 +0200

    Update interface

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -73,7 +73,7 @@ const Sidebar = ({ isMobile, isOpen, onClose }: { isMobile?: boolean; isOpen?: b
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
-        Compose
+        New Message
       </button>
 
       {/* Navigation */}
