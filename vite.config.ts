9a8ed6af19a250780cc327a14e298284682457de
Liam Von Astoria <liam.dispa@skygenesisenterprise.com>
2025-05-15 14:47:52 +0200

    Mise à jour de nodemon et de Vite (#18)

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+    plugins: [
+        react(),
+        {
+            name: "custom-message", // Nom du plugin
+            configureServer(server) {
+                console.log("\x1b[36m%s\x1b[0m", "🚀 Vite server is starting..."); // Message personnalisé en cyan
+                console.log("\x1b[32m%s\x1b[0m", "🌟 Aether Mail Frontend is ready to go!");
+            },
+            buildStart() {
+                console.log("\x1b[33m%s\x1b[0m", "📦 Building Aether Mail Frontend...");
+            },
+        },
+    ],
 });
